71056d816d15788cb3daebaa86022f6a3d201ce7
john-denny <johndenny116@gmail.com>
2025-02-23 11:32:27 +0100

    Moved in chrome extension

--- chrome_extention/background.js
+++
@@ -1,6 +0,0 @@
-//open popup when user navigates to the page
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.url.includes("https://www.ros.ie/myreceipts-web/ros/receipt/expense-details/add")) {
-        chrome.action.openPopup(); 
-    }
-});
